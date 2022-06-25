6551a65f5be0c19dfe18ebf19b9e0ad566f96fdc
PolinaMokretsova <pmokretsova@mail.ru>
2022-06-25 13:27:48 +0500

    edit date of birth element

--- tests/test_form.py
+++ tests/test_form.py
@@ -12,7 +12,10 @@ def test_submit_form():
    browser.element('#userEmail').should(be.blank).type('Polina@polina.com')
    browser.element('[for="gender-radio-2"]').click()
    browser.element('#userNumber').should(be.blank).type('8123456789')
-   browser.element('#dateOfBirthInput').set_value('25 jun 1990')
+   browser.element('#dateOfBirthInput').click()
+   browser.element('[value="1999"]').click()
+   browser.element('[value="6"]').click()
+   browser.element('div[aria-label="Choose Monday, July 26th, 1999"]').click()
    browser.element('#subjectsInput').should(be.blank).type('English').press_enter()
    browser.element('[for="hobbies-checkbox-1"]').click()
    browser.element('#uploadPicture').type("/Users/mokretsova/PycharmProjects/demoqa_tests/tests/котик.png")
@@ -28,7 +31,7 @@ def test_submit_form():
       'Student Name Polina Mokretsova',
       'Student Email Polina@polina.com',
       'Gender Female', 'Mobile 8123456789',
-      'Date of Birth 25 June,202225',
+      'Date of Birth 26 July,1999',
       'Subjects English',
       'Hobbies Sports',
       'Picture котик.png',
