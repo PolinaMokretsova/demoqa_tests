8eef2ea2c5c955943175b0d6d3a953c579580a72
PolinaMokretsova <pmokretsova@mail.ru>
2022-06-28 12:53:57 +0500

    add 4 spase before code

--- tests/test_form.py
+++ tests/test_form.py
@@ -1,42 +1,43 @@
+import pathlib
 from selene.support.shared import browser
 from selene import have
 
 
 
-
 def test_submit_form():
-   browser.open('/automation-practice-form')
+    browser.open('/automation-practice-form')
 
 
-   browser.element('#firstName').type('Polina')
-   browser.element('#lastName').type('Mokretsova')
-   browser.element('#userEmail').type('Polina@polina.com')
+    browser.element('#firstName').type('Polina')
+    browser.element('#lastName').type('Mokretsova')
+    browser.element('#userEmail').type('Polina@polina.com')
 
-   browser.element('[for="gender-radio-2"]').click()
+    browser.element('[for="gender-radio-2"]').click()
 
-   browser.element('#userNumber').type('8123456789')
+    browser.element('#userNumber').type('8123456789')
 
-   browser.element('#dateOfBirthInput').click()
-   browser.element('[value="1999"]').click()
-   browser.element('[value="6"]').click()
-   browser.element('div[aria-label="Choose Monday, July 26th, 1999"]').click()
+    browser.element('#dateOfBirthInput').click()
+    browser.element('[value="1999"]').click()
+    browser.element('[value="6"]').click()
+    browser.element('div[aria-label="Choose Monday, July 26th, 1999"]').click()
 
-   browser.element('#subjectsInput').type('English').press_enter()
+    browser.element('#subjectsInput').type('English').press_enter()
 
-   browser.element('[for="hobbies-checkbox-1"]').click()
+    browser.element('[for="hobbies-checkbox-1"]').click()
 
-   browser.element('#uploadPicture').type("/Users/mokretsova/PycharmProjects/demoqa_tests/tests/котик.png")
+    browser.element('#uploadPicture').send_keys(path/)
+      #.type("/Users/mokretsova/PycharmProjects/demoqa_tests/tests/котик.png")
 
-   browser.element('#currentAddress').type('Yekatetinburg')
+    browser.element('#currentAddress').type('Yekatetinburg')
 
-   browser.element('#react-select-3-input').type('Haryana').press_enter()
-   browser.element('#react-select-4-input').type('Karnal').press_enter()
+    browser.element('#react-select-3-input').type('Haryana').press_enter()
+    browser.element('#react-select-4-input').type('Karnal').press_enter()
 
-   browser.element('#submit').click()
+    browser.element('#submit').click()
 
 
-   browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
-   browser.all('[class="table table-dark table-striped table-bordered table-hover"] tr').should(have.exact_texts(
+    browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
+    browser.all('[class="table table-dark table-striped table-bordered table-hover"] tr').should(have.exact_texts(
       'Label Values',
       'Student Name Polina Mokretsova',
       'Student Email Polina@polina.com',
