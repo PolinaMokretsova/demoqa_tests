9980d92bbb045513bc992a68ef593b8a1d58227f
PolinaMokretsova <pmokretsova@mail.ru>
2022-07-27 18:19:19 +0500

    edit fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,7 +11,7 @@ from demoqa_tests.utils import attach
 def browser_config():
     browser.config.base_url = 'https://demoqa.com'
     browser.config.browser_name = 'chrome'
-    browser.config.window_width = 500
+    browser.config.window_width = 350
     browser.config._window_height = 1000
 
 
